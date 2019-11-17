3dff8d4a8652bb2dc6218b39d1e9bb159fbd582f
robzelluf <robzelluf@hotmail.com>
2019-11-17 13:15:14 +0200

    Load model info and change reward

--- pong_train.py
+++ pong_train.py
@@ -19,7 +19,8 @@ warnings.filterwarnings("ignore")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--headless", action="store_true", help="Run in headless mode")
-parser.add_argument("--fps", type=int, help="FPS for rendering", default=30)
+parser.add_argument("--fps", type=int, help="FPS for rendering", default=2000)
+parser.add_argument("--glie_a", type=int, help="GLIE-a value", default=30)
 parser.add_argument("--scale", type=int, help="Scale of the rendered game", default=1)
 parser.add_argument("--load", action="store_true")
 args = parser.parse_args()
@@ -41,9 +42,10 @@ start_episode = 0
 if args.load:
     player.target_net = torch.load("models/target_net.pth")
     player.policy_net = torch.load("models/policy_net.pth")
-    start_episode = 200
+    with open("models/model_info.p", "rb") as f:
+        start_episode = pickle.load(f)
 
-glie_a = 2000
+glie_a = args.glie_a
 
 # Set the names for both SimpleAIs
 env.set_names(player.get_name(), opponent.get_name())
@@ -57,6 +59,7 @@ for i in range(start_episode, episodes):
     state, _ = env.reset()
     state = np.transpose(state)
     state_diff = state - state
+    point = 0
     while not done:
         # Get the actions from both SimpleAIs
         action1 = player.get_action(state, eps)
@@ -78,6 +81,7 @@ for i in range(start_episode, episodes):
         # Count the wins
         if rew1 == 10:
             win1 += 1
+            point = 1
         if i % 10 == 0 and not args.headless:
             env.render()
         if done:
@@ -85,7 +89,7 @@ for i in range(start_episode, episodes):
             print("episode {} over. Broken WR: {:.3f}".format(i, win1/(i+1)))
             print("Epsilon:", eps)
 
-    cumulative_rewards.append(0.9 * cumulative_rewards[-1] + 0.1 * win1)
+    cumulative_rewards.append(0.9 * cumulative_rewards[-1] + 0.1 * point)
     print("Last average reward:", cumulative_rewards[-1])
     if not args.headless:
         plot_rewards(cumulative_rewards)
@@ -94,5 +98,5 @@ for i in range(start_episode, episodes):
         print("Models saved!")
         torch.save(player.policy_net, "models/policy_net.pth")
         torch.save(player.target_net, "models/target_net.pth")
-        # with open("models/model_info.p", "wb") as f:
-        #     pickle.dump(i, f)
+        with open("models/model_info.p", "wb") as f:
+            pickle.dump(i, f)
