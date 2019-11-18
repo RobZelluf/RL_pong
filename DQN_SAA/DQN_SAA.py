f56bfa357404a260fd36f366540fe596129fd3bd
robzelluf <robzelluf@hotmail.com>
2019-11-18 21:24:41 +0200

    Fix DQN

--- DQN_SAA/DQN_SAA.py
+++ DQN_SAA/DQN_SAA.py
@@ -15,8 +15,8 @@ class Q_CNN(nn.Module):
         self.action_space = action_space
 
         self.conv1 = nn.Conv2d(1, 2, 1, 1)
-        self.pool = torch.nn.MaxPool2d(kernel_size=4, stride=4)
-        self.fc1 = torch.nn.Linear(50 * 50 * 2, 64)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = torch.nn.Linear(100 * 100 * 2, 64)
         self.fc2 = torch.nn.Linear(64, action_space)
 
     def forward(self, x):
@@ -30,7 +30,7 @@ class Q_CNN(nn.Module):
         # Reshape data to input to the input layer of the neural net
         # Size changes from (18, 16, 16) to (1, 4608)
         # Recall that the -1 infers this dimension from the other given dimension
-        x = x.view(-1, 50 * 50 * 2)
+        x = x.view(-1, 100 * 100 * 2)
 
         # Computes the activation of the first fully connected layer
         # Size changes from (1, 4608) to (1, 64)
