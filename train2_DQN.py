b637ca2417089460ef45df4b7c94455557c4afd0
robzelluf <robzelluf@hotmail.com>
2019-12-04 12:03:33 +0200

    Reload and save to new train2

--- train2_DQN.py
+++ train2_DQN.py
@@ -38,26 +38,41 @@ episodes = 500000
 player1_id = 1
 player2_id = 3 - player1_id
 
-if input("Continue training on 2v2 agent? (y/n)") == "y":
-    with open("DQN_SAA/two_agents/model_info.p", "rb") as f:
-        print("Continue training 2v2!")
-        model_info = pickle.load(f)
+DIRs = [x for x in os.listdir("DQN_SAA/") if os.path.isdir("DQN_SAA/" + x) and "cache" not in x]
+i = 0
+for DIR in DIRs:
+    print(i, DIR)
+    i += 1
 
-else:
-    with open("DQN_SAA/bigger-CNN/model_info.p", "rb") as f:
-        model_info = pickle.load(f)
+model_ind = int(input("Model number:"))
+model_name = DIRs[model_ind]
+
+with open("DQN_SAA/" + model_name + "/model_info.p", "rb") as f:
+    model_info = pickle.load(f)
+
+new_model = input("Save as new model? (n/model_name)")
 
 player1 = DQN_SAA(env, player1_id, model_info=model_info, fc1_size=args.fc1_size)
 player2 = DQN_SAA(env, player2_id, model_info=model_info, fc1_size=args.fc1_size)
 
-new_capacity = int(player1.memory.capacity / 2)
+if new_model != "n":
+    model_name = new_model
+    model_info["model_name"] = model_name
+    if not os.path.exists("DQN_SAA/" + model_name):
+        os.mkdir("DQN_SAA/" + model_name)
+
+    with open("DQN_SAA/" + model_name + "/model_info.p", "wb") as f:
+        pickle.dump(model_info, f)
+
+ignore_opponent = False
+if "ignore_opponent" in model_info:
+    ignore_opponent = model_info["ignore_opponent"]
+
+print("Ignoring opponent", ignore_opponent)
+
+new_capacity = int(player1.memory.capacity / 3)
 player1.memory.capacity = new_capacity
 player2.memory.capacity = new_capacity
-
-if model_info["model_name"] is not "two_agents":
-    model_info["model_name"] = "two_agents"
-    with open("DQN_SAA/two_agents/model_info.p", "wb") as f:
-        pickle.dump(model_info, f)
 
 glie_a = args.glie_a
 target_update = args.target_update
@@ -83,8 +98,8 @@ for i in range(1, episodes):
 
     state1, state2 = env.reset()
 
-    state1 = process_state(state1, player1.size)
-    state2 = process_state(state2, player2.size)
+    state1 = process_state(state1, player1.size, ignore_opponent)
+    state2 = process_state(state2, player2.size, ignore_opponent)
     state_diff1 = 2 * state1 - state1
     state_diff2 = 2 * state2 - state2
 
@@ -109,8 +124,8 @@ for i in range(1, episodes):
         # Step the environment and get the rewards and new observations
         (next_state1, next_state2), (rew1, rew2), done, info = env.step((action1, action2))
 
-        next_state1 = process_state(next_state1, player1.size)
-        next_state2 = process_state(next_state2, player2.size)
+        next_state1 = process_state(next_state1, player1.size, ignore_opponent)
+        next_state2 = process_state(next_state2, player2.size, ignore_opponent)
 
         next_state_diff1 = 2 * next_state1 - state1
         next_state_diff2 = 2 * next_state2 - state2
@@ -166,18 +181,18 @@ for i in range(1, episodes):
         print("Action distribution:", list(chosen_actions))
 
         if args.save:
-            with open("DQN_SAA/two_agents/rewards.p", "wb") as f:
+            with open("DQN_SAA/" + model_name + "/rewards.p", "wb") as f:
                 pickle.dump(rewards, f)
 
             if np.mean(wins) > np.mean(wins2):
-                torch.save(player1.policy_net, "DQN_SAA/two_agents/policy_net.pth")
+                torch.save(player1.policy_net, "DQN_SAA/" + model_name + "/policy_net.pth")
                 player2.policy_net = player1.policy_net
                 print("Model 1 saved!")
             else:
-                torch.save(player2.policy_net, "DQN_SAA/two_agents/policy_net.pth")
+                torch.save(player2.policy_net, "DQN_SAA/" + model_name + "/policy_net.pth")
                 player1.policy_net = player2.policy_net
                 print("Model 2 saved!")
 
-            with open("DQN_SAA/two_agents/performance.txt", "a") as f:
+            with open("DQN_SAA/" + model_name + "/performance.txt", "a") as f:
                 f.write("episode {} over. RWR1: {:.3f}. RWR2 {:.3f}. RAA: {:.3f}.".format(i, np.mean(wins), np.mean(wins2), RA_actions))
                 f.write("\n")
